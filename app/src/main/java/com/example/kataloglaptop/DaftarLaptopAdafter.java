97d8e1f01aa948b8339cc1ba7f0a6cd741aa414f
octaviananova <octaviananova729@gmail.com>
2021-01-13 16:32:22 +0700

    Katalog Laptop

---
+++ app/src/main/java/com/example/kataloglaptop/DaftarLaptopAdafter.java
@@ -0,0 +1,55 @@
+package com.example.kataloglaptop;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.example.kataloglaptop.GenericUtility;
+import com.example.kataloglaptop.R;
+import com.example.kataloglaptop.model.Laptop;
+
+import java.util.List;
+
+public class DaftarLaptopAdafter extends ArrayAdapter<Laptop> {
+    Context context;
+
+    public DaftarLaptopAdafter(@NonNull Context context, @NonNull List<Laptop> objects) {
+        super(context, R.layout.row_laptop, objects);
+        this.context = context;
+    }
+
+    class ViewHolder {
+        TextView txNilai;
+        TextView txDeskripsi;
+        TextView txModel;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Laptop tr = getItem(position);
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.row_laptop,parent,false);
+            viewHolder = new ViewHolder();
+            viewHolder.txDeskripsi = convertView.findViewById(R.id.row_tx_desc_transaksi);
+            viewHolder.txNilai = convertView.findViewById(R.id.row_tx_nilai_transaksi);
+            viewHolder.txModel = convertView.findViewById(R.id.row_tx_nilai_model);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.txDeskripsi.setText(tr.getDeskripsi());
+        viewHolder.txModel.setText(tr.getModel());
+        if (tr.getJenis().equals(Laptop.ASUS)) {
+            viewHolder.txNilai.setText("-"+GenericUtility.formatUang(tr.getNilai()));
+        } else {
+            viewHolder.txNilai.setText(GenericUtility.formatUang(tr.getNilai()));
+        }
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/kataloglaptop/model/Laptop.java
@@ -0,0 +1,96 @@
+package com.example.kataloglaptop.model;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+
+public class Laptop {
+    public static final String ASUS="ASUS";
+    public static final String ACER="ACER";
+    public static final String HP="HP";
+    public static final String THOSIBA="THOSIBA";
+    public static final String LENOVO="LENOVO";
+    public static final String APPLE="APPLE";
+    private String id;
+    private String deskripsi;
+    private double nilai;
+    private String jenis;
+    private String model;
+
+    public Laptop() {
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public Double getNilai() {
+        return nilai;
+    }
+
+    public void setNilai(double nilai) {
+        this.nilai = nilai;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getModel(){
+        return model;
+    }
+    public void setModel( String model){
+        this.model = model;
+    }
+
+    public static Laptop fromJSONObject(JSONObject obj) {
+        Laptop tr = new Laptop();
+        try {
+            tr.setId(obj.getString("id"));
+            tr.setDeskripsi(obj.getString("deskripsi"));
+            tr.setNilai(obj.getDouble("nilai"));
+            tr.setJenis(obj.getString("jenis"));
+            tr.setModel(obj.getString("model"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return tr;
+    }
+
+    public JSONObject toJSONObject() {
+        JSONObject jsonObj = new JSONObject();
+        try {
+            jsonObj.put("id",this.id);
+            jsonObj.put("jenis",this.jenis);
+            jsonObj.put("nilai",this.nilai);
+            jsonObj.put("deskripsi",this.deskripsi);
+            jsonObj.put("model",this.model);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObj;
+    }
+}
